7747fdd94b27c8dd3708c1100c58ec8e3ad3508c
hyesuhan <heasoo88@gmail.com>
2024-12-19 17:20:02 +0900

    [feat] Swagger API 설정 (JWTx)

---
+++ src/main/java/Career/server/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package Career.server.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Career-For-Me") // API의 제목
+                .description("커리어포미 Swagger UI") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
