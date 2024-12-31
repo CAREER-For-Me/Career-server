cc6ca2b4e41bd07355eef72ec46d2434769b2a2f
Dlrjswn <lkj6387@gmail.com>
2025-01-01 01:47:09 +0900

    [FEAT] Field, Posting 엔티티 생성

---
+++ src/main/java/Career/server/domain/enums/CorporateType.java
@@ -0,0 +1,5 @@
+package Career.server.domain.enums;
+
+public enum CorporateType {
+    BIG,STARTUP,GONG,MIDDLE,FOREIGN,NONPROFIT,CLUB,ELSE;
+}
---
+++ src/main/java/Career/server/repository/FieldRepository.java
@@ -0,0 +1,9 @@
+package Career.server.repository;
+
+import Career.server.domain.mapping.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FieldRepository extends JpaRepository<Field, Long> {
+}
